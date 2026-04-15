2d23494ade353e4ff0bd0620aa8e91316fbb6581
Mehedi Hasan <developerhasan99@gmail.com>
2026-04-15 23:13:51 +0600

    chore: update Vite configuration to customize output file names for build assets

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/openmarket-frontend/",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/index.css",
+        entryFileNames: "assets/index.js",
+      },
+    },
+  },
 });
